6f1c56d93b13359e08712cbc93ae17459f044cac
Antony D <Antony D@AntonyDavid.AD>
2017-06-05 12:51:22 -0500

    añadiendo codigo java script en otro documento .js

---
+++ js/cambioimg.js
@@ -0,0 +1,11 @@
+
+function pageload() {
+	var img = document.getElementById("img");
+	img.onclick = changeimg;
+}
+function changeimage() {
+	var webimg = document.getElementById("icono1");
+	webimg.src = "/img/js.png";
+
+}
+window.onload = pageload;
