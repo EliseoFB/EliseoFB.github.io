79a9ec4d227afacec455dd33644e78a8354d6fbe
Antony D <Antony D@AntonyDavid.AD>
2017-06-10 11:54:02 -0500

    ver centros turisticos

---
+++ js/turisticos.js
@@ -0,0 +1,13 @@
+function pageload() {
+	var inicio = document.getElementById("navegacion");
+	inicio.onclick = myFunction;
+}
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+window.onload = pageload;
