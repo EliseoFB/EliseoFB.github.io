c57f3868cfb37807986943d8b4118d7236e5a68a
Antony D <Antony D@AntonyDavid.AD>
2017-06-12 07:10:32 -0500

    ver index

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+/* agregar mapa de cajamarca*/
 function myMap() {
 	  var myCenter = new google.maps.LatLng(-7.1637800,-78.5002700);
 	  var mapCanvas = document.getElementById("map");
@@ -12,3 +12,24 @@ function myMap() {
 	  });
 	  infowindow.open(map,marker);
 	}
+/* agregar mapa de nav*/
+
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card-2" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
