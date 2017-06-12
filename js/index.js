28b79732687f1759cff05b6f2c62f6a6fdda3796
Antony D <Antony D@AntonyDavid.AD>
2017-06-12 07:21:43 -0500

    modal

--- js/index.js
+++ js/index.js
@@ -12,34 +12,21 @@ function myMap() {
 	  });
 	  infowindow.open(map,marker);
 	}
-/* agregar mapa de slideshow*/
+/* agregar mapa de cajamarca*/
 
-<!-- Sctip de slideshow -->
-<script>
-	var slideIndex = 1;
-	showDivs(slideIndex);
+	document.getElementsByClassName("tablink")[0].click();
 
-	function plusDivs(n) {
-  showDivs(slideIndex += n);
-	}
-
-	function currentDiv(n) {
-		showDivs(slideIndex = n);
-	}
-
-	function showDivs(n) {
- 	var i;
-		var x = document.getElementsByClassName("mySlides");
-		var dots = document.getElementsByClassName("demo");
-		if (n > x.length) {slideIndex = 1}    
-		if (n < 1) {slideIndex = x.length}
-		for (i = 0; i < x.length; i++) {
- 	x[i].style.display = "none";  
-		}
-		for (i = 0; i < dots.length; i++) {
- 	dots[i].className = dots[i].className.replace(" w3-red", "");
-		}
-		x[slideIndex-1].style.display = "block";  
-		dots[slideIndex-1].className += " w3-red";
-	}
-	</script>
+		function openCity(evt, cityName) {
+		  var i, x, tablinks;
+				 x = document.getElementsByClassName("city");
+				for (i = 0; i < x.length; i++) {
+				  x[i].style.display = "none";
+					 }
+				tablinks = document.getElementsByClassName("tablink");
+				for (i = 0; i < x.length; i++) {
+					tablinks[i].classList.remove("w3-light-grey");
+					 }
+				 document.getElementById(cityName).style.display = "block";
+				evt.currentTarget.classList.add("w3-light-grey");
+				}
+			
